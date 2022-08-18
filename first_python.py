c7023e90d3caf5748a0858042116cbf617de98f7
msuresh <111486738+msuresh1822@users.noreply.github.com>
2022-08-18 12:42:55 +0530

    Update first_python.py

--- first_python.py
+++ first_python.py
@@ -1,6 +1,3 @@
 import streamlit
 streamlit.title('Hi, Good Morning Family')
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.menu('omega 3 & Blueberry oatmeal')
-streamlit.menu('kale,Spinach & Rocket Smoothie')
