ec228e24592f3417436be602ad96ef73659909bd
msuresh <111486738+msuresh1822@users.noreply.github.com>
2022-08-18 13:03:13 +0530

    Update first_python.py
    added emojis

--- first_python.py
+++ first_python.py
@@ -1,10 +1,10 @@
 import streamlit
 streamlit.title('Hi, Good Morning Family')
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry oatmeal')
-streamlit.text('kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard_Boiled Free_range Egg')
+streamlit.header('❤️Breakfast Menu')
+streamlit.text('🍇omega 3 & Blueberry oatmeal')
+streamlit.text('🥬kale,Spinach & Rocket Smoothie')
+streamlit.text('🐔	Hard_Boiled Free_range Egg')
 streamlit.text("Avocado Toast")
 streamlit.header('Build your own fruit smoothie')
 
